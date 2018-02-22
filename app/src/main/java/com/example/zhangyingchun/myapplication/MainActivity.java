fc783b1c3a8ea4784b031a3831817ca9b60afbfc
zhangyingchun08 <zhangyingchun08@qq.com>
2018-02-22 15:07:23 +0800

    zyc测试

---
+++ app/src/main/java/com/example/zhangyingchun/myapplication/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.zhangyingchun.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.zhangyingchun.myapplication.utils.MyTextUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        myHello();
+
+        for (int counter = 0; counter <10 ; counter++) {
+            Log.i("MainActivity","Value:"+counter);
+        }
+    }
+
+    private void myHello() {
+        MyTextUtil myUtil= new MyTextUtil();
+        TextView mtv= findViewById(R.id.textView);
+        mtv.setText(myUtil.getValue());
+    }
+
+}
---
+++ app/src/main/java/com/example/zhangyingchun/myapplication/utils/MyTextUtil.java
@@ -0,0 +1,11 @@
+package com.example.zhangyingchun.myapplication.utils;
+
+/**
+ * Created by zhangyingchun on 2018/2/22.
+ */
+
+public class MyTextUtil {
+    public String getValue() {
+        return "hello from utils.MyTextUtil";
+    }
+}
